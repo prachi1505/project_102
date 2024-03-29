412b810dbffcb5eff9c846d0ae6ee9d9e6daee8e
prachi1505 <85737452+prachi1505@users.noreply.github.com>
2024-03-29 20:44:24 +0530

    Add files via upload

---
+++ 102/Move_File.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+from_dir = "C:\Users\PRACHI\Downloads"
+to_dir = "C:\Users\PRACHI\Desktop\Doc(102)"
+
+list_of_files = os.listdir(from_dir) 
+
+for file_name in list_of_files:
+    name , extension = os.path.splitext(file_name)
+
+    if extension == '':
+        continue
+    if extension in ['.txt','.pdf','.doc','.docx']:
+        
+        path1 = from_dir + "/" + file_name
+        path2 = to_dir+ "/" + "Doc(102)"
+        path3 = to_dir + "/" + "Doc(102)" + file_name
+
+        if os.path.exists(path2):
+            print("Moving" + file_name + "......")
+            shutil.move(path1,path3)
+
+        else:
+            os.makedirs(path2)
+            print("Moving" + file_name + ".......")
+            shutil.move(path1,path3)    
+
+        
+
